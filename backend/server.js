611ad8fe829b3e9a327864a8c242929fb2fbdeb8
Rana Walid <ranawalidabdelrazek297@gmail.com>
2026-08-28 17:16:21 +0300

    installed cors

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
+
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
 require("dotenv").config();
 
 
 const app = express();
+app.use(cors());
 
 const PORT = 5000;
 
